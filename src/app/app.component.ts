1e6124e503e9d65bbd5f29c92260bc5cfe922de6
r_castilho <rafaelcastilhodacosta@gmail.com>
2024-11-21 14:16:03 -0300

    feat(implementation-validate-fields): validate fields login atv #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'cristoFy';
