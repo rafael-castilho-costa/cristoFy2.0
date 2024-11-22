4efc035d8b070565e2c4a1790316a0429a6a7371
r_castilho <rafaelcastilhodacosta@gmail.com>
2024-11-22 09:25:19 -0300

    feat(part-2): part 2 validate fields login atv #5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { headerComponent } from './header/header.component';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,10 @@ export const routes: Routes = [
       {
         path:'login',
         component:LoginComponent
+      },
+      {
+        path:'header',
+        component:headerComponent
       }
     ],
   },
