0b50729baafa4a3a845558f46f22408a51763dd3
r_castilho <rafaelcastilhodacosta@gmail.com>
2024-12-12 17:50:22 -0300

    fix(15-adjust-routers): Project Router Outlet atv #15

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,28 +9,33 @@ import { VisitantesComponent } from './visitantes/visitantes.component';
 import { MembrosComponent } from './membros/membros.component';
 
 export const routes: Routes = [
+      {
+        path:'',
+        component: fullComponent,
 
-      {
-        path: '', redirectTo: 'login', pathMatch: 'full',
-      },
-      {
-        path:'full', component: fullComponent,
-      },
-      {
-        path: 'home', component: HomeComponent,
-      },
-      {
-        path:'login', component:LoginComponent,
-      },
-      {
-        path:'agenda', component: AgendaComponent,
-      },
-      {
-        path:'visitantes', component: VisitantesComponent,
-      },
-      {
-        path:'membros', component: MembrosComponent,
-      },
+        children: [
+          {
+            path:'',
+            redirectTo:'home',
+            pathMatch:'full'
+          },
+          {
+            path: 'home', component: HomeComponent,
+          },
+          {
+            path:'login', component:LoginComponent,
+          },
+          {
+            path:'agenda', component: AgendaComponent,
+          },
+          {
+            path:'visitantes', component: VisitantesComponent,
+          },
+          {
+            path:'membros', component: MembrosComponent,
+          },
+        ]
+      }
 ];
 
 @NgModule({
